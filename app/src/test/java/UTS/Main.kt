9600e41827d6de35cb21dd49cbc85403ed608953
fadillahda <lalafadillah1105@gmail.com>
2024-03-23 21:37:53 +0700

    UTS

--- app/src/test/java/UTS/Main.kt
+++ app/src/test/java/UTS/Main.kt
@@ -1,7 +1,7 @@
 package UTS
 
-fun main() {
-    // Inisialisasi mutableList dan tambah data List
+fun  main() {
+
     val clubs = mutableListOf<Club>()
     clubs.add(Club("Liverpool", 19, 8, 9, 6, 3))
     clubs.add(Club("Manchester United", 20, 12, 6, 3, 1))
@@ -9,38 +9,32 @@ fun main() {
     clubs.add(Club("Manchester City", 8, 8, 8, 0, 0))
     clubs.add(Club("Arsenal", 13, 14, 2, 0, 0))
 
-    // Passing a named function ::sortByNumberTrophyTotal
+
     val sortedClubs = filterAndSort(clubs, ::sortByNumberTrophyTotal)
-    // Hasil sorting total trofi
     println("Urutan klub berdasarkan jumlah total trofi: ")
     for (club in sortedClubs) {
         println("${club.name}: ${club.totalTrophy} trofi")
     }
 
-    // Passing a named function ::filterByEuropeanTrophy
+
     val filteredClubs = filterAndSort(clubs, ::filterByEuropeanTrophy)
-    // Hasil sorting total trofi
     println("\nKlub yang belum pernah memenangkan UCL dan UEL:")
     for (club in filteredClubs) {
         println(club.name)
     }
 
-    // Panggil extension function Club recap
     println("\nRekap perolehan trofi klub:")
     clubs.forEach { println(it.recap()) }
 }
 
-// High order function filter and sort
 fun filterAndSort(clubs: List<Club>, options: (List<Club>) -> List<Club>): List<Club> {
     return options(clubs)
 }
 
-// Fungsi sorting
 fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
     return clubs.sortedByDescending { it.totalTrophy }
 }
 
-// Fungsi filter
 fun filterByEuropeanTrophy(clubs: List<Club>): List<Club> {
     return clubs.filter { it.ucl == 0 && it.uel == 0 }
 }
